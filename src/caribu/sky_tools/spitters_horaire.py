52c5990d8894773ff96afa3e5ea201e79cb9320b
Christian Fournier <christian.fournier@inra.fr>
2013-07-25 12:48:47 +0000

    update spitters model
    git-svn-id: https://scm.gforge.inria.fr/authscm/revesansparole/svn/openaleapkg/trunk/caribu@2969 0978ab36-7443-0410-8248-c2aa344a765a

--- src/caribu/sky_tools/spitters_horaire.py
+++ src/caribu/sky_tools/spitters_horaire.py
@@ -1,6 +1,55 @@
 ############ G Louarn - adaptation de spitters.c EGC grignon
 from math import *
 
+def DecliSun(DOY):
+    """ Declinaison (rad) du soleil en fonction du jour de l'annee """
+    alpha = 2 * pi * (DOY - 1) / 365
+    return (0.006918 - 0.399912 * cos(alpha) + 0.070257 * sin(alpha))
+
+def DayLength(latitude, decli):
+    """ photoperiode (radians) en fonction de latitude (degre) et declinaison du soleil (rad) """
+    lat = radians(latitude)
+    d = acos(-tan(decli) * tan(lat))
+    if d < 0:
+        d = d + pi    
+    return 2 * d
+
+def dH(angleH):
+    """ duration (hour) from  daylength angle (radians)"""
+    return 24 / (2 * pi) * angleH
+    
+def extra(Rg, DOY, heureTU, latitude):
+    """ rayonnement extraterrestre horarire """
+    hrad = 2 * pi / 24 * (heureTU - 12)
+    lat = radians(latitude)
+    dec = DecliSun (DOY)
+    costheta = sin(lat) * sin(dec) + cos(lat) * cos(dec) * cos(hrad)
+    Io = 1370 * (1 + 0.033 * cos(2 * pi * (DOY - 4) / 366))#eclairement (w/m2) a la limitte de l'atmosphere dans un plan perpendiculaire aux rayons du soleil, fonction du jour
+    So = Io * costheta #eclairement dans un plan parallele a la surface du sol
+    return So
+
+def RdRsH(Rg, DOY, heureTU, latitude):
+    """ fraction diffus/Global en fonction du rapport Global(Sgd)/Extraterrestre(Sod)- pas de temps horaire """
+    hrad = 2 * pi / 24 * (heureTU - 12)
+    lat = radians(latitude)
+    dec = self.DecliSun(DOY)
+    costheta = sin(lat) * sin(dec) + cos(lat) * cos(dec) * cos(hrad)
+    Io = 1370 * (1 + 0.033 * cos(2 * pi * (DOY - 4) / 366))#eclairement (w/m2) a la limitte de l'atmosphere dans un plan perpendiculaire aux rayons du soleil, fonction du jour
+    So = Io * costheta #eclairement dans un plan parallele a la surface du sol
+    RsRso = Rg / So
+    R = 0.847 - 1.61 * costheta + 1.04 * costheta * costheta
+    K = (1.47 - R) / 1.66
+    
+    if (RsRso <= 0.22) :
+        return(1)
+    elif (RsRso <= 0.35) :
+        return(1 - 6.4 * (RsRso - 0.22)**2)
+    elif (RsRso <= K) :
+        return(1.47 - 1.66 * RsRso)
+    else:
+        return(R)
+
+
 
 class spitters_horaire(object):
     """  Doc... """ 
@@ -24,61 +73,23 @@ class spitters_horaire(object):
     
     def DecliSun (self,DOY):
         """ Declinaison (rad) du soleil en fonction du jour de l'annee """
-        alpha=2*3.14*(DOY-1)/365
-        return (0.006918-0.399912*cos(alpha)+0.070257*sin(alpha))
-    
+        return DecliSun(DOY)    
+
     def DayLength (self,latitude,decli):
         """ photoperiode en fonction de latitude (degre) et declinaison du soleil (rad) """
-        lat=radians(latitude)
-        d=acos(-tan(decli)*tan(lat))
-        if d<0:
-            d=d+3.14
+        return DayLength(latitude,decli)
         
-        return 2*d
-    
     def extra (self,Rg,DOY,heureTU,latitude):
         """ rayonnement extraterrestre horarire """
-        hrad=2*3.14/24*(heureTU-12)
-        lat=radians(latitude)
-        dec=self.DecliSun (DOY)
-        costheta=sin(lat)*sin(dec)+cos(lat)*cos(dec)*cos(hrad)
-        Io=1370*(1+0.033*cos(2*3.14*(DOY-4)/366))#eclairement (w/m2) a la limitte de l'atmosphere dans un plan perpendiculaire aux rayons du soleil, fonction du jour
-        So=Io*costheta #eclairement dans un plan parallele a la surface du sol
-        return So
-    
+        return extra(Rg,DOY,heureTU,latitude)
+        
     def RdRsH (self,Rg,DOY,heureTU,latitude):
         """ fraction diffus/Global en fonction du rapport Global(Sgd)/Extraterrestre(Sod)- pas de temps horaire """
-        hrad=2*3.14/24*(heureTU-12)
-        lat=radians(latitude)
-        dec=self.DecliSun (DOY)
-        costheta=sin(lat)*sin(dec)+cos(lat)*cos(dec)*cos(hrad)
-        Io=1370*(1+0.033*cos(2*3.14*(DOY-4)/366))#eclairement (w/m2) a la limitte de l'atmosphere dans un plan perpendiculaire aux rayons du soleil, fonction du jour
-        So=Io*costheta #eclairement dans un plan parallele a la surface du sol
-        RsRso=Rg/So
-        R=0.847-1.61*costheta+1.04*costheta*costheta
-        K=(1.47-R)/1.66
-        
-        if (RsRso<=0.22) :
-            return(1)
-        else:
-            if (RsRso<=0.35) :
-                return(1-6.4*(RsRso-0.22)*(RsRso-0.22))
-            else:
-                if (RsRso<=K) :
-                    return(1.47-1.66*RsRso)
-                else:
-                    return(R)
-            
-
-
+        return RdRsH(Rg,DOY,heureTU,latitude)
 #
 # Formule pour avoir Rg horraire a partir de Rg journalier (Kaplanis, 2005.Renewable energy, 31:781:790)
 #
 #
-def DecliSun (DOY):
-    """ Declinaison (rad) du soleil en fonction du jour de l'annee """
-    alpha=2*3.14*(DOY-1)/365
-    return (0.006918-0.399912*cos(alpha)+0.070257*sin(alpha))
 
 def RgH (Rg,hTU,DOY,latitude) :
     """ compute hourly value of Rg at hour hTU for a given day at a given latitude
