ecb742ec67b0ff838aee5d85509e6eacf7c6acba
revesansparole <revesansparole@gmai.com>
2016-05-13 14:26:09 +0200

    function prototypes for caribu python

--- src/alinea/caribu/caribu.py
+++ src/alinea/caribu/caribu.py
@@ -28,5 +28,35 @@ def raycasting(triangles, materials, lights, domain=None, screen_size=1536):
     del screen_size
 
 
-def radiosity():
-    pass
+def radiosity(triangles, materials, lights, domain=None, mixed_radiosity=None,
+              screen_size=1536):
+    """Compute triangles illumination using radiosity model.
+
+    Args:
+        triangles:  (list of triangles) Each triangle is defined as
+                   a triplet of oriented points in 3D space.
+        materials: (list of optical materials)  # TODO
+        lights: (list of lights)
+        domain: (tuple of floats) Domain for repetition of the scene
+                 (xmin, ymin, xmax, ymax) scene is not bounded along z axis
+                 if None (default), scene is not repeated
+        mixed_radiosity: (float, int, float) parameters used for mixed
+                         radiosity computation (default None, pure radiosity)
+                         or (radius, nb_layers, height):
+                          - radius: distance for radiosity effects
+                          - nb_layers: vertical subdivisions of scene
+                          - height: maximum height of scene
+        screen_size: (int) buffer size for projection images
+
+    Returns:
+        (dict of str:property) properties computed:
+          - Ei
+          - Ei_min
+          - Ei_max
+    """
+    del triangles
+    del materials
+    del lights
+    del domain
+    del mixed_radiosity
+    del screen_size
