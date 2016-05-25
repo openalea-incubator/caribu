fc9edffd51ad2af581e41a84b23872dbf79ff819
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2016-05-25 13:41:45 +0200

    add caribu_shell tests

---
+++ test/test_caribu_shell.py
@@ -0,0 +1,22 @@
+""" Unit Tests for caribu_shell module """
+
+from alinea.caribu.caribu_shell import caribu_run_case, CaribuOptionError
+
+
+# Original test of caribu.csh script by M. Chelle
+def test_caribu_script():
+    for i in range(1, 6):
+        yield caribu_run_case, i  # this makes nose generate 5 tests
+
+
+def test_caribu_script_inconsistent():
+    for i in range(1, 3):
+        yield caribu_run_inconsistent_case, i
+
+
+def caribu_run_inconsistent_case(i):
+    try:
+        caribu_run_case(-i)
+        assert False, "This test uses inconsistent options, it should raise an CaribuOptionError"
+    except CaribuOptionError:
+        assert True
