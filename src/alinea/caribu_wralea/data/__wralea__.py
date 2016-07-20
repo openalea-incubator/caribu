aeefbd6e39969865c1d7df20d42b5880142f507c
revesansparole <revesansparole@gmail.com>
2016-07-20 16:28:02 +0200

    cleaned wralea and added uid to factories

--- src/alinea/caribu_wralea/data/__wralea__.py
+++ src/alinea/caribu_wralea/data/__wralea__.py
@@ -1,8 +1,6 @@
-
 # This file has been generated at Thu Oct 27 16:38:56 2011
 
-from openalea.core import *
-
+from openalea.core import DataFactory as DF
 
 __name__ = 'alinea.caribu.data'
 
@@ -16,115 +14,113 @@ __authors__ = 'C. Fournier'
 __institutes__ = 'INRA'
 __icon__ = ''
 
-
 __all__ = ['_63495120', '_63495280', '_63494992', '_63495088', '_63495312',
            '_63494928', '_63495248', '_63495152', '_63495216', '_63495024',
            '_63495184', '_63495056', '_114779792', 'sky_tools_meteo',
            'sky_tools_Rg2J']
 
+sky_tools_meteo = DF(uid="f02588164e8311e6bff6d4bed973e64a",
+                     name='Meteo2006.csv',
+                     description='',
+                     editors=None,
+                     includes=None,
+                     )
 
-sky_tools_meteo = DataFactory(name='Meteo2006.csv',
+sky_tools_Rg2J = DF(uid="f02588174e8311e6bff6d4bed973e64a",
+                    name='Rg_2J.csv',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
-sky_tools_Rg2J = DataFactory(name='Rg_2J.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
+_63495120 = DF(uid="f02588184e8311e6bff6d4bed973e64a",
+               name='f331s1_100plantes.can',
+               description='',
+               editors=None,
+               includes=None,
+               )
 
+_63495280 = DF(uid="f02588194e8311e6bff6d4bed973e64a",
+               name='CanestraDoc.pdf',
+               description='Mini Documentation for Caribu',
+               editors=None,
+               includes=None,
+               )
 
-_63495120 = DataFactory(name='f331s1_100plantes.can',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
+_63494992 = DF(uid="f025881a4e8311e6bff6d4bed973e64a",
+               name='Turtle16uoc.light',
+               description='',
+               editors=None,
+               includes=None,
+               )
 
+_63495088 = DF(uid="f025881b4e8311e6bff6d4bed973e64a",
+               name='Turtle16soc.light',
+               description='',
+               editors=None,
+               includes=None,
+               )
 
-_63495280 = DataFactory(name='CanestraDoc.pdf',
-                    description='Mini Documentation for Caribu',
-                    editors=None,
-                    includes=None,
-                    )
+_63495312 = DF(uid="f025881c4e8311e6bff6d4bed973e64a",
+               name='zenith.light',
+               description='',
+               editors=None,
+               includes=None,
+               )
 
+_63494928 = DF(uid="f025881d4e8311e6bff6d4bed973e64a",
+               name='maize.8',
+               description='',
+               editors=None,
+               includes=None,
+               )
 
-_63494992 = DataFactory(name='Turtle16uoc.light',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
+_63495248 = DF(uid="f025881e4e8311e6bff6d4bed973e64a",
+               name='filterT.can',
+               description='',
+               editors=None,
+               includes=None,
+               )
 
+_63495152 = DF(uid="f025881f4e8311e6bff6d4bed973e64a",
+               name='Turtle46soc.light',
+               description='',
+               editors=None,
+               includes=None,
+               )
 
-_63495088 = DataFactory(name='Turtle16soc.light',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
+_63495216 = DF(uid="f02588204e8311e6bff6d4bed973e64a",
+               name='soc.light',
+               description='',
+               editors=None,
+               includes=None,
+               )
 
+_63495024 = DF(uid="f02588214e8311e6bff6d4bed973e64a",
+               name='filter.8',
+               description='',
+               editors=None,
+               includes=None,
+               )
 
-_63495312 = DataFactory(name='zenith.light',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
+_63495184 = DF(uid="f0dd52de4e8311e6bff6d4bed973e64a",
+               name='Turtle46uoc.light',
+               description='',
+               editors=None,
+               includes=None,
+               )
 
+_63495056 = DF(uid="f0dd52df4e8311e6bff6d4bed973e64a",
+               name='par.opt',
+               description='',
+               editors=None,
+               includes=None,
+               )
 
-_63494928 = DataFactory(name='maize.8',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_63495248 = DataFactory(name='filterT.can',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_63495152 = DataFactory(name='Turtle46soc.light',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_63495216 = DataFactory(name='soc.light',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_63495024 = DataFactory(name='filter.8',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_63495184 = DataFactory(name='Turtle46uoc.light',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_63495056 = DataFactory(name='par.opt',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_114779792 = DataFactory(name='par4.opt',
-                    description='optical properties with 4 species compatible for ear representation in ADEL',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-
+_114779792 = DF(uid="f0dd52e04e8311e6bff6d4bed973e64a",
+                name='par4.opt',
+                description=('optical properties with 4 species compatible '
+                             'for ear representation in ADEL'),
+                editors=None,
+                includes=None,
+                )
