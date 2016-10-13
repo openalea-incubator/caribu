8404c704a2725ce8e9e76117545026e029e45107
pradal <christophe.pradal@inria.fr>
2016-10-13 18:22:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,8 +43,8 @@ setup_kwds = dict(
     version=version["__version__"],
     description=short_descr,
     long_description=readme + '\n\n' + history,
-    author="Christian Fournier, Michael Chelle, ",
-    author_email="Christian.Fournier@supagro.inra.fr, michael.chelle@grignon.inra.fr, ",
+    author="Christian Fournier, Michael Chelle, Christophe Pradal ",
+    author_email="Christian.Fournier@supagro.inra.fr, michael.chelle@grignon.inra.fr, christophe dot pradal _at_ cirad fr ",
     url='https://github.com/openalea-incubator/caribu',
     license='INRA_License_agreement',
     zip_safe=False,
@@ -72,9 +72,10 @@ setup_kwds = dict(
 # #}
 # change setup_kwds below before the next pkglts tag
 
-#build_prefix = "build-scons"
-#setup_kwds['scons_scripts'] = ['SConstruct']
-#setup_kwds['bin_dirs'] = {'bin': build_prefix + '/bin'}
+setup_kwds['setup_requires'] = ['openalea.deploy']
+build_prefix = "build-scons"
+setup_kwds['scons_scripts'] = ['SConstruct']
+setup_kwds['bin_dirs'] = {'bin': build_prefix + '/bin'}
 setup_kwds['entry_points']['wralea'] = ['alinea.caribu = alinea.caribu_wralea']
 setup_kwds['entry_points']["console_scripts"] = []
 setup_kwds['package_data'][''] = ['*.can', '*.R', '*.8', '*.opt', '*.light', '*.csv', '*.png','*.pyd', '*.so', '*.dylib']
