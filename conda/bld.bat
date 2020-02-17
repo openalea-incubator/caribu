847818ca4424da510d2c78f5bdc651a9cac8df8d
Frederic Boudon <frederic.boudon@cirad.fr>
2020-02-17 17:04:55 +0100

    compil with py3 and fredboudon channel

--- conda/bld.bat
+++ conda/bld.bat
@@ -2,6 +2,10 @@ COPY options_conda_win.py options.py
 
 REM systeminfo
 
+scons -j ${CPU_COUNT} 
+MOVE build-scons\bin\* $PREFIX\bin
+MOVE build-scons\lib\* $PREFIX\lib
+
 %PYTHON% setup.py install
 
 if errorlevel 1 exit 1
