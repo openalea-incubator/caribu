857ab4e4ae09c3342155a1cb0dabdbfacced54cc
Frederic Boudon <frederic.boudon@cirad.fr>
2020-03-26 22:19:33 +0100

    add deploy

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,6 @@
-scons -j ${CPU_COUNT} CC=$CC  CCFLAGS=$CCFLAGS
-mv build-scons/bin/* $PREFIX/bin
-mv build-scons/lib/* $PREFIX/lib
+scons -j ${CPU_COUNT} CC=$CC  CCFLAGS=$CCFLAGS install
+#mv build-scons/bin/* $PREFIX/bin
+#mv build-scons/lib/* $PREFIX/lib
 #mkdir $PREFIX/include/caribu
 #mv build-scons/include/* $PREFIX/include/caribu
 $PYTHON setup.py install
